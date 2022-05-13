19443eda605fe0a6e33a3da25c0af56a89ddee83
Liam Callahan <53151930+nahallac5@users.noreply.github.com>
2022-05-13 13:31:23 -0400

    v0.1 complete
    You can run to completion, selects time and events randomly over a given distance while keeping track of days

--- Main.py
+++ Main.py
@@ -1,24 +1,66 @@
-import matplotlib as mp
-import pandas as pd
+import random
 from resources import roadside_encounter
 
 # Travel Class to handle distance modifcations
 class Travel:
     def __init__(self, dist):
-        self.totalDistance = dist
+        self.totalDistance    = int(dist)
+        self.distanceTraveled = 0
+        self.daysTraveled     = 0
+        # Flag for program completion
+        self.travelcomplete   = 0
         
+    # Move forward and see if encounter triggers
+    def action(self):
+        incriment = input(f"Day {self.daysTraveled} of travel. Distance traveled today (miles): ")
+        # Add distance for the day
+        self.distanceTraveled += int(incriment)
+        # Check for exit condition
+        if (self.distanceTraveled >= self.totalDistance):
+            self.travelcomplete = 1
+            print(f"Travel complete.\nDays traveled: {self.daysTraveled}\n Miles traveled: {self.totalDistance}\n")
+        # Check for event
+        else:
+            encounterSeed = random.randrange(100)
+            if encounterSeed <= 30:
+                # Picks a random event from the roadside encounters
+                eventApprove = 0
+                while eventApprove == 0:
+                    # Pick time:
+                    timeRand = random.randrange(100)
+                    eventTime = ""
+                    if (timeRand <= 15):
+                        eventTime = "Morning"
+                    elif(timeRand > 15 and timeRand <= 85):
+                        eventTime = "Afternoon"
+                    else:
+                        eventTime = "Night"
+                    # Pick event:
+                    encounterPrompt = random.choice(list(roadside_encounter.event.values()))
+                    print(f"Time: {eventTime}\nEvent: {encounterPrompt}\n")
+                    # Approve generated event, else reroll
+                    eventInquery = input("Approve? (y/n): ")
+                    if (eventInquery == "y"):
+                        eventApprove = 1
+            else:
+                print(f"Day {self.daysTraveled} passes by uneventfully.")
+            # Incriment days
+            self.daysTraveled += 1
+                
+            
 
 def main():
     # Run on open
     travelDone = 0
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    print("Welcome to the Pathfinder Travel Tool.")
+    print("Welcome to the Pathfinder Travel Tool (v0.1).")
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
     # Set up trip
     trip = Travel(travelDistInput())
     
-    #while travelDone == 0:
-    #    pass
+    # Execute Travel loop
+    while trip.travelcomplete == 0:
+        trip.action()
     
 
 # User Input to 
@@ -32,7 +74,7 @@ def travelDistInput():
             # Make sure its in int form, not str
             travelDistance = int(travelDistance)
             # Travel Estimate
-            print(f"Estimated Travel time for {travelDistance}mi:\n30ft max(24mi/day): {travelDistance/24} days\n20ft(16mi/day) max: {travelDistance/16}\n")
+            print(f"Estimated Travel time for {travelDistance}mi:\n30ft(24mi/day): {round(travelDistance/24, 2)} days\n20ft(16mi/day): {round(travelDistance/16, 2)}\n")
             travelConfrim = input("Does this sound right? (y/n): ")
             # Conformation
             if (travelConfrim == 'y'):
@@ -42,4 +84,5 @@ def travelDistInput():
     
     return travelDistance
 
+# Run
 main()
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # pathfinder_travel
 Small program used to handle different aspects of travel in pathfinder
+
+# V0.1 - 5/13/2022
+Features:
+ - Takes in and stores travel distance
+ - Can manually inciment distance X miles of days
+ - Rolls for encounter when milage is incrimented. Only Event/RP encounters are currently included
+ - If an encounter is chosen, time of day for encounter is also displayed
